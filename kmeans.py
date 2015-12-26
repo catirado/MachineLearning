864e725bc03b186936f97c6dc8cf2759ca3f6ad4
Carlos Tirado <catirado87@gmail.com>
2015-12-26 08:45:14 +0100

    initial commit

---
+++ kmeans.py
@@ -0,0 +1,4 @@
+#https://gist.github.com/iandanforth/5862470
+#http://codereview.stackexchange.com/questions/80050/k-means-clustering-algorithm-in-python
+#http://stanford.edu/~cpiech/cs221/handouts/kmeans.html
+#http://pandoricweb.tumblr.com/post/8646701677/python-implementation-of-the-k-means-clustering
---
+++ knn.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+#http://insideourminds.net/python-simple-k-nearest-neighbours-classifier/
+#http://machinelearningmastery.com/tutorial-to-implement-k-nearest-neighbors-in-python-from-scratch/
+#http://stackoverflow.com/questions/20052041/k-nearest-neighbours-algorithm-python
+#http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_ml/py_knn/py_knn_understanding/py_knn_understanding.html
+#https://www.youtube.com/watch?v=8pTlCJX59Do
+#http://blog.yhathq.com/posts/classification-using-knn-and-python.html
+#https://github.com/scikit-learn/scikit-learn/blob/86b1ba72771718acbd1e07fbdc5caaf65ae65440/sklearn/neighbors/classification.py
