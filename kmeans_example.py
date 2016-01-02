4bd5dea71108d0419be7322e6c0e19dbed38f56b
Carlos Tirado <catirado87@gmail.com>
2016-01-02 18:20:37 +0100

    example

--- kmeans_example.py
+++ kmeans_example.py
@@ -2,7 +2,28 @@ import kmeans
 import arff
 
 
-if __name__ == '__main__':
+def flores_clustering_data_set_run():
     data_set = arff.load(open('./data/flores_clustering.arff'))
-    results = kmeans.run(data_set['data'], 100)
-    print(results)
+    k = 3
+    results = kmeans.run(data_set['data'], k)
+    print "Centroids"
+    print results[0]
+    print "Clusters"
+    print results[1]
+    print "Resolved in " + str(results[2]) + " iterations"
+
+
+def baloncesto_data_set_run():
+    data_set = arff.load(open('./data/baloncesto.arff'))
+    k = 3
+    results = kmeans.run(data_set['data'], k)
+    print "Centroids"
+    print results[0]
+    print "Clusters"
+    print results[1]
+    print "Resolved in " + str(results[2]) + " iterations"
+
+
+if __name__ == '__main__':
+    flores_clustering_data_set_run()
+    baloncesto_data_set_run()
